5bf6b58ce408bc02b2f24eba725b3f2c735ac74c
Emilio Ruscitti <emiliorccp@gmail.com>
2024-01-14 04:24:19 -0300

    feat: Modulized git

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,25 +1,44 @@
-{...}: {
-  enable = true;
-  userName = "Emilio Ruscitti";
-  userEmail = "emiliorccp@gmail.com";
-  aliases = {
-    aa = "add .";
-    co = "checkout";
-    b = "branch";
-    cc = "commit";
-    pm = "push origin main";
-    pp = "push origin";
-    mm = "merge";
-    st = "status";
-    pl = "pull";
-  };
-  delta = {
-    enable = true;
-    options = {
-      decorations = {};
-      features = "decorations";
-      whitespace-error-style = "22 reverse";
-      syntax-theme = "Nord";
+{pkgs, ...}: let
+  cfg = cfg.zengit;
+in {
+  options.zengit = with pkgs.lib; {
+    enable = mkEnableOption "Enable zengit config and packages";
+    userName = mkOption {
+      type = types.str;
     };
+    userEmail = mkOption {
+      type = types.str;
+    };
+  };
+  config = pkgs.lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userName = cfg.userName;
+      userEmail = cfg.userEmail;
+      aliases = {
+        aa = "add .";
+        co = "checkout";
+        b = "branch";
+        cc = "commit";
+        pm = "push origin main";
+        pp = "push origin";
+        mm = "merge";
+        st = "status";
+        pl = "pull";
+      };
+      delta = {
+        enable = true;
+        options = {
+          decorations = {};
+          features = "decorations";
+          whitespace-error-style = "22 reverse";
+          syntax-theme = "Nord";
+        };
+      };
+    };
+    home.packages = with pkgs; [
+      git
+      gitoxide
+    ];
   };
 }
