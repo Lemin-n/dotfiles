275150740316a304e246acfb0a29ce8368280ba2
Emilio Ruscitti <emiliorccp@gmail.com>
2024-01-14 04:25:22 -0300

    fix: Delete useless pkgs in systemPackages

--- modules/environment.nix
+++ modules/environment.nix
@@ -6,64 +6,16 @@
     ];
   };
   environment.systemPackages = with pkgs; [
-    cargo-shuttle
-    wl-clipboard
-    google-chrome
-    carapace
-    spot
-    # Remote and Networking
-    anydesk
-    winbox
-
-    # Social
-    slack
-
-    # Spotify
-    spotify-tui
-    spotifyd
-    spotify
-
     # Default
     curl
+    ripgrep
+    git
     wget
     pkg-config
-
-    # SSL
     openssl
-    openssl.dev
-
-    # Windows tools
     ntfs3g
-
-    # Compression
     zip
     unzip
     gnutar
-    dbeaver
-
-    # Predefined
-    # Git
-    git
-    gitoxide
-
-    # Bluetooth
-    bluez
-
-    # Utils
-    ripgrep
-    gcc_multi
-
-    # Xserver
-    xdg-utils
-    pavucontrol
-
-    # Docker
-    docker-compose
-
-    # Font
-    font-manager
-
-    #Hyprland
-    wev
   ];
 }
