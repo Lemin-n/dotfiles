c5c1472d959c9808fc638e7940664077c4ff2d08
Emilio Ruscitti <emiliorccp@gmail.com>
2024-01-15 21:00:53 -0300

    chore: Modularized options in `zentube` module

--- modules/youtube/default.nix
+++ modules/youtube/default.nix
@@ -4,20 +4,15 @@
   ...
 }: let
   cfg = config.zentube;
-  youtube-packages = with pkgs.lib.list; flatten [optionals cfg.tui [youtube-tui youtube-dl-light libsixel mpv-unwrapped] optionals cfg.music [youtube-music]];
+  tui-pkgs = with pkgs; lib.lists.optionals cfg.tui [youtube-tui youtube-dl-light libsixel mpv-unwrapped];
+  music-pkgs = with pkgs; lib.lists.optional cfg.music youtube-music;
+  youtube-packages = with pkgs.lib.lists; flatten [tui-pkgs music-pkgs];
+  zentube-options = import ./module.nix (pkgs.lib);
 in {
-  options.zentube = with pkgs.lib; {
-    enable = mkOptionEnable "Enable Zentube packages";
-    tui = mkOption {
-      type = types.bool;
-      default = true;
-    };
-    music = mkOption {
-      type = types.bool;
-      default = false;
-    };
-  };
-  config = pkgs.lib.mkIf cfg.enable {
+  options.zentube =
+    zentube-options;
+
+  config = {
     home.packages = youtube-packages;
   };
 }
