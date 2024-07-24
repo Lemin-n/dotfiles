8405460ec32096345c2892b490e4a9147c6ef03d
Emilio Ruscitti <emiliorccp@gmail.com>
2024-07-24 02:16:24 -0300

    fix(youtube): delete insecure package youtube-dl-light

--- modules/youtube/default.nix
+++ modules/youtube/default.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   cfg = config.zentube;
-  tui-pkgs = with pkgs; lib.lists.optionals cfg.tui [youtube-tui youtube-dl-light libsixel mpv-unwrapped];
+  tui-pkgs = with pkgs; lib.lists.optionals cfg.tui [youtube-tui libsixel mpv-unwrapped];
   music-pkgs = with pkgs; lib.lists.optional cfg.music youtube-music;
   youtube-packages = with pkgs.lib.lists; flatten [tui-pkgs music-pkgs];
   zentube-options = import ./module.nix (pkgs.lib);
