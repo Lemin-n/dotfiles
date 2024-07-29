7f8cb177831a858dacbbe3aef64a459e4e0e7a79
Emilio Ruscitti <emiliorccp@gmail.com>
2024-07-29 02:28:25 -0300

    chore(nvim): disable `inlay_hint` on `rustraceanvim` attach hook

--- modules/nvim/plugins/rustaceanvim.lua
+++ modules/nvim/plugins/rustaceanvim.lua
@@ -25,7 +25,7 @@ vim.g.rustaceanvim = {
 	},
 	server = {
 		on_attach = function(client, bufnr)
-			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+			--		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 		end,
 		default_settings = {
 			["rust-analyzer"] = {
