a051d35cbd8cf5c86b8788e9bbae80bbaf9e6b04
Emilio Ruscitti <emiliorccp@gmail.com>
2024-08-26 09:03:15 -0300

    fix(nvim): temporary solution to volar-lsp

--- modules/nvim/plugins/cmp.nix
+++ modules/nvim/plugins/cmp.nix
@@ -2,5 +2,30 @@
 with pkgs.vimPlugins; {
   plugin = lsp-zero-nvim;
   type = "lua";
-  config = pkgs.lib.strings.fileContents ./cmp.lua;
+  config = pkgs.lib.strings.fileContents ./cmp.lua + "
+lsp.tsserver.setup({
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = '${pkgs.lib.getExe pkgs.vue-language-server}',
+        languages = { 'vue' },
+      },
+    },
+  },
+  on_attach = function(client)
+	client.server_capabilities.documentFormattingProvider = false
+  end,
+})
+lsp.volar.setup({
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  cmd = { '${pkgs.lib.getExe pkgs.vue-language-server}', '--stdio' },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
+})
+lsp_zero.setup()
+";
 }
