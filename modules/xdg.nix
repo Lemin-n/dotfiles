ef85fd141a2f16ff01546425df6211f70e6eb0d1
Emilio Ruscitti <emiliorccp@gmail.com>
2025-03-05 11:27:57 -0300

    chore(portals): Disable gtk as default portal

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -6,7 +6,7 @@
     };
     extraPortals = [pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-kde pkgs.xdg-desktop-portal-gnome pkgs.xdg-desktop-portal-wlr];
     config.common = {
-      default = "gtk";
+      #default = "gtk";
     };
   };
 }
